da763e9b8be4cc1f92b2785d7948358b0f2f77fc
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2020-11-07 19:18:53 -0800

    Updates

---
+++ precache-manifest.b17e551492c0703460c99d79b084a4eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd0cd235df81be075f4d290728ee0ad",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "ae324b6e5ac8db229786",
+    "url": "/react-test/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "12c1f040ccef7395156d",
+    "url": "/react-test/static/css/main.8a39cf22.chunk.css"
+  },
+  {
+    "revision": "ae324b6e5ac8db229786",
+    "url": "/react-test/static/js/2.a5ce0a72.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/react-test/static/js/2.a5ce0a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c1f040ccef7395156d",
+    "url": "/react-test/static/js/main.90f45c26.chunk.js"
+  },
+  {
+    "revision": "a73b77d85a0436678cfa",
+    "url": "/react-test/static/js/runtime-main.a3de2790.js"
+  }
+]);
